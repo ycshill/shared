62162526c4cd12010f32f24a7610ecf2e0eabf62
ycshill <shilili1992@gmail.com>
2018-12-18 17:43:01 +0800

    feat: flutter-share jspang_demo 实现卡片不

--- flutter-share/jspang_demo/lib/main.dart
+++ flutter-share/jspang_demo/lib/main.dart
@@ -3,31 +3,61 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var card = new Card(
+    child: Column(
+      children: <Widget>[
+        ListTile(
+          title: Text(
+            '吉林省吉林市昌邑区',
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          subtitle: Text(
+            '技术胖:1513938888',
+          ),
+          leading: Icon(
+            Icons.account_box,
+            color: Colors.lightBlue,
+          ),
+        ),
+        new Divider(),
+        ListTile(
+          title: new Text(
+            '北京市海淀区中国科技大学',
+            style: TextStyle(fontWeight: FontWeight.w500),
+          ),
+          subtitle: new Text('胜宏宇:1513938888'),
+          leading: new Icon(
+            Icons.account_box,
+            color: Colors.lightBlue,
+          ),
+        ),
+        new Divider(),
+        ListTile(
+          title: new Text(
+            '河南省濮阳市百姓办公楼',
+            style: TextStyle(fontWeight: FontWeight.w500),
+          ),
+          subtitle: new Text('JSPang:1513938888'),
+          leading: new Icon(
+            Icons.account_box,
+            color: Colors.lightBlue,
+          ),
+        ),
+      ],
+    ),
+  );
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '图片相册',
+      title: '卡片布局',
       home: Scaffold(
-        body: GridView(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            // 行与行之间的距离
-            mainAxisSpacing: 2,
-            // 列与列之间的距离
-            crossAxisSpacing: 2,
-            childAspectRatio: 0.7,
-          ),
-          children: <Widget>[
-            new Image.network('http://img5.mtime.cn/mt/2018/10/22/104316.77318635_180X260X4.jpg',fit: BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mt/2018/10/10/112514.30587089_180X260X4.jpg',fit: BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mt/2018/11/13/093605.61422332_180X260X4.jpg',fit: BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mt/2018/11/07/092515.55805319_180X260X4.jpg',fit: BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mt/2018/11/21/090246.16772408_135X190X4.jpg',fit: BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mt/2018/11/17/162028.94879602_135X190X4.jpg',fit: BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mt/2018/11/19/165350.52237320_135X190X4.jpg',fit: BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mt/2018/11/16/115256.24365160_180X260X4.jpg',fit: BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mt/2018/11/20/141608.71613590_135X190X4.jpg',fit: BoxFit.cover),
-          ],
+        appBar: AppBar(
+          title: Text('卡片布局'),
+        ),
+        body: Center(
+          child: card,
         ),
       ),
     );
