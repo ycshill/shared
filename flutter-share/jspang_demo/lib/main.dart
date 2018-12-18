f4951fe44cab82017ce768fe4c24cb7493fc8528
ycshill <shilili1992@gmail.com>
2018-12-18 15:48:30 +0800

    feat: flutter-share jspang_demo 实现一个相册库

--- flutter-share/jspang_demo/lib/main.dart
+++ flutter-share/jspang_demo/lib/main.dart
@@ -6,21 +6,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GridView',
+      title: '图片相册',
       home: Scaffold(
-        body: GridView.count(
-          padding: const EdgeInsets.all(20),
-          // 网格间的空档
-          crossAxisSpacing: 3,
-          // 网格的列数
-          crossAxisCount: 3,
+        body: GridView(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            // 行与行之间的距离
+            mainAxisSpacing: 2,
+            // 列与列之间的距离
+            crossAxisSpacing: 2,
+            childAspectRatio: 0.7,
+          ),
           children: <Widget>[
-            const Text('I am Jspang'),
-            const Text('I love Web'),
-            const Text('jspang.com'),
-            const Text('我喜欢玩游戏'),
-            const Text('我喜欢看书'),
-            const Text('我喜欢吃火锅')
+            new Image.network('http://img5.mtime.cn/mt/2018/10/22/104316.77318635_180X260X4.jpg',fit: BoxFit.cover),
+            new Image.network('http://img5.mtime.cn/mt/2018/10/10/112514.30587089_180X260X4.jpg',fit: BoxFit.cover),
+            new Image.network('http://img5.mtime.cn/mt/2018/11/13/093605.61422332_180X260X4.jpg',fit: BoxFit.cover),
+            new Image.network('http://img5.mtime.cn/mt/2018/11/07/092515.55805319_180X260X4.jpg',fit: BoxFit.cover),
+            new Image.network('http://img5.mtime.cn/mt/2018/11/21/090246.16772408_135X190X4.jpg',fit: BoxFit.cover),
+            new Image.network('http://img5.mtime.cn/mt/2018/11/17/162028.94879602_135X190X4.jpg',fit: BoxFit.cover),
+            new Image.network('http://img5.mtime.cn/mt/2018/11/19/165350.52237320_135X190X4.jpg',fit: BoxFit.cover),
+            new Image.network('http://img5.mtime.cn/mt/2018/11/16/115256.24365160_180X260X4.jpg',fit: BoxFit.cover),
+            new Image.network('http://img5.mtime.cn/mt/2018/11/20/141608.71613590_135X190X4.jpg',fit: BoxFit.cover),
           ],
         ),
       ),
