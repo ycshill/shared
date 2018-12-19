6ac40ca4040d778102b654beb76509287274980d
ycshill <shilili1992@gmail.com>
2018-12-19 20:40:40 +0800

    feat: 引入dart的分享

---
+++ flutter-share/dart/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dart 的基础语法练习'),
+      ),
+      body: Center(
+        child: Text('1212'),
+      ),
+    );
+  }
+}
+
+
+void main() {
+  runApp(MaterialApp(
+    title: 'dart 的基础语法',
+    home: FirstPage(),
+  ));
+
+  print('hello dart');
+}
