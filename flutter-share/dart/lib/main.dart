40bbea800618c275c301a3cdbf36f0194bbefcd8
ycshill <shilili1992@gmail.com>
2018-12-20 16:58:50 +0800

    feat: dynamic

--- flutter-share/dart/lib/main.dart
+++ flutter-share/dart/lib/main.dart
@@ -22,42 +22,13 @@ void main() {
     home: FirstPage(),
   ));
 
-  // 创建Map
-  Map a = {
-    'name': 'lily',
-    'age': '26',
-  };
-  print(a); // {name: lily, age: 26}
-  // 创建一个不可变的map
-  Map b = const {
-    'name': 'lily',
-    'age': '26',
-  };
-  // b['name'] = 'xiaoming'; // Cannot set value in unmodifiable Map
-  print(b);
-  Map c = new Map();
-  print(c); // {}
+  dynamic a = 0;
+  print(a); // 0
+  a = 'hello';
+  print(a); // hello
 
-  // 常用的操作
-  Map d = {
-    'name': 'lily',
-    'age': '26',
-  };
-  print(d['name']); // lily
-  d['name'] = 'xiao';
-  print(d); // {name: xiao, age: 26}
-  print(d.length); // 2
-  print(d.isNotEmpty); // true
-  print(d.isEmpty); // false
-  print(d.keys); // (name, age)
-  print(d.values);  // (xiao, 26)
-  print(d.containsKey('name')); // true
-  print(d.containsValue(3)); // false
-  d.remove('name');
-  print(d); // {age: 26}
-  d.forEach(getKeyValue); //key-age, value-26
-}
-
-void getKeyValue(key, value) {
-  print('key-$key, value-$value');
+  List arr = new List<dynamic>();
+  arr.add(1);
+  arr.add('hello');
+  print(arr); // [1, hello]
 }
