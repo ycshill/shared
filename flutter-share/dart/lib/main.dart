cf83b01fb6b64f5164ec00f86cf11f0db6cfe7b8
ycshill <shilili1992@gmail.com>
2018-12-27 17:37:49 +0800

    feat: mixin

--- flutter-share/dart/lib/main.dart
+++ flutter-share/dart/lib/main.dart
@@ -22,39 +22,18 @@ void main() {
     home: FirstPage(),
   ));
 
-  offsetPoint dot = offsetPoint(1, 2, 10);
-  print(dot); // offsetPoint{x=11, y=12}
-  print(dot.x); // 11 ，调用get
-  print(dot.y); // 12
-  dot.x = 4;  // 调用set
-  print(dot); // offsetPoint{x=14, y=12}
+  Man p1 = new Man('lily');
+  p1.eat();  // i eat
 }
 
-class offsetPoint{
-  int _x;
-  int _y;
-  int offset;
-
-  offsetPoint(int x, int y, int offset)
-    : _x = x,
-      _y = y,
-      offset = offset {
-        print('初始化实例变量');
-      }
-
-  // 定义一个getter
-  int get x => _x + offset;
-  // getter 不能有参数，连括号都要删掉
-  int get y {
-    return _y + offset;
+mixin Person {
+  String name;
+  eat() {
+    print('i eat');
   }
+}
 
-  // 定义setter
-  void set x(int x) => _x = x;
-  void set y(int y) => _y = y;
-
-  @override
-  String toString() {
-    return 'offsetPoint{x=$x, y=$y}';
-  }
+class Man with Person {
+  String name;
+  Man(this.name);
 }
